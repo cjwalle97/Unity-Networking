5a1df718b929fae78e319c7f2c5b201ea4025321
Christopher Walle <cjwalle97@gmail.com>
2018-01-19 17:40:50 -0600

    Update
    Update

---
+++ Assets/Scripts/Name.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Name : NetworkBehaviour
+{
+    [SyncVar(hook = "InputName")]
+    public string PlayerName = "Test";
+
+
+
+    void InputName(string newname)
+    {
+        if(!isServer)
+        {
+            return;
+        }
+        PlayerName = newname;
+        Debug.Log("Name Changed");
+    }
+}
